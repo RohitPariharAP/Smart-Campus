a5ea55fc58b934d9e581eae86888cb8298f19b9a
RohitPariharAP <kailashparihar7053@gmail.com>
2025-04-19 20:37:34 +0530

    Update API base URL and environment variables

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'import.meta.env.VITE_API_URL/api', // Point to your backend server
+  baseURL: 'import.meta.env.VITE_API_URL', // Point to your backend server
   headers: {
     'Content-Type': 'application/json'
   }
